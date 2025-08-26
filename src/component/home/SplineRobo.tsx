d34916c133d9671d266633468a231ecab1962807
DevAshan <ashanwithanarachchi@gmail.com>
2025-08-26 13:47:55 +0530

    Add spline robot UI

---
+++ src/component/home/SplineRobo.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Spline from '@splinetool/react-spline';
+
+const SplineRobo = () => {
+    return (
+        <>
+
+            <div className='bg-red-300 w-20 h-20'>
+                <Spline
+                    scene="https://prod.spline.design/Bsk3y3-ep8BPJiwJ/scene.splinecode"
+                 />
+            </div>
+
+        </>
+    )
+}
+
+export default SplineRobo
