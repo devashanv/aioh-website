80449370f75da3a22b03d4d80b4929d00c3e4f88
DevAshan <ashanwithanarachchi@gmail.com>
2025-08-26 21:39:10 +0530

    Add service ribbon UI

--- src/component/home/SplineRobo.tsx
+++ src/component/home/SplineRobo.tsx
@@ -5,7 +5,7 @@ const SplineRobo = () => {
     return (
         <>
 
-            <div className='bg-red-300 w-20 h-20'>
+            <div className='bg-red-300 w-100 h-100'>
                 <Spline
                     scene="https://prod.spline.design/Bsk3y3-ep8BPJiwJ/scene.splinecode"
                  />
