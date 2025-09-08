f18c23115ffc1a9de7b2eb76f38d005782f0ef96
prabhathkaluarachchi <fmprabhath@gmail.com>
2025-09-08 12:54:41 +0530

    Issues fix mobile navigation menu and other

--- src/component/common/FooterTop.tsx
+++ src/component/common/FooterTop.tsx
@@ -6,12 +6,15 @@ const FooterTop: React.FC = () => {
   return (
     <>
       <div className="relative p-3">
-        <div className="relative text-center text-2xl lg:text-[48px] font-semibold">
-          <p className="">Need A Boost For Your Business?</p>
-          <p className="text-center text-[18px] text-[#191818] font-light mt-1"> Lets Connect With Us</p>
+        {/* Content + button */}
+        <div className="relative text-center text-2xl lg:text-[48px] font-semibold z-50">
+          <p>Need A Boost For Your Business?</p>
+          <p className="text-center text-[18px] text-[#191818] font-light mt-1">
+            Lets Connect With Us
+          </p>
 
           <button
-            className="lg:inline-block px-5 py-2 lg:py-3 text-lg font-normal py-0 rounded-[30px] text-white whitespace-nowrap cursor-pointer mt-4 z-40"
+            className="lg:inline-block px-5 py-2 lg:py-3 text-lg font-normal rounded-[30px] text-white whitespace-nowrap cursor-pointer mt-4"
             style={{ background: gradient }}
             onClick={() => (window.location.href = "/contact")}
           >
@@ -24,20 +27,21 @@ const FooterTop: React.FC = () => {
             style={{
               width: "clamp(200px, 15vw, 300px)",
               height: "clamp(200px, 15vw, 300px)",
-            }}>
+            }}
+          >
             <div className="w-full h-full">
               <Spline scene="https://prod.spline.design/Bsk3y3-ep8BPJiwJ/scene.splinecode" />
             </div>
           </div>
         </div>
 
+        {/* Footer Line image (unchanged style, just z fixed) */}
         <div className="flex z-10 absolute items-center justify-center bottom-10 right-0 w-1/2">
-          <div className="relative flex w-full pr-25 z-0">
-            {/* Footer Line image */}
+          <div className="relative flex w-full pr-25">
             <img
               src={FooterLine}
               alt="line"
-              className="hidden md:block w-full px-5 -z-1"
+              className="hidden lg:block w-full px-5"
             />
           </div>
         </div>
